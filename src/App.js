c52fc6c162085ec5b87403ea8600547f3e7d3929
Simon Achkar <simon@simonachkar.com>
2021-11-24 21:06:15 -0400

    added basic styling

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome <code>human</code>. Login to display your information.
         </p>
-        <a
-          className="App-link"
+        <button
+          className="App-button"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Login
+        </button>
       </header>
     </div>
   );
