60aac8d13dc7d0e88728d7714a7695c1a43f592f
Simon Achkar <simon@simonachkar.com>
2021-11-24 21:50:04 -0400

    Added toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Toggle from './Toggle';
 
 function App() {
+  const [isDark, toggleDark] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome <code>human</code>. Login to display your information.
-        </p>
-        <button
-          className="App-button"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login
-        </button>
-      </header>
+    <div className={isDark ? "app-dark" : "app-light"}>
+      <Toggle onClick={() => toggleDark(!isDark)} />
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Welcome <code>human</code>. Login to display your information.
+      </p>
+      <button
+        className="login-button"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Login
+      </button>
+
+
+
     </div>
   );
 }
